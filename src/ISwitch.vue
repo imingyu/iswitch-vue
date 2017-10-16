45e6d06f1cfea80c1875034cc364c51311d24c27
imingyu <ymy48253@tcent.cn>
2017-10-16 19:23:06 +0800

    add file

---
+++ src/ISwitch.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="iswitch-container">
+        <label class="isw-control">
+            <input type="checkbox" class="isw-checkbox">
+            <div class="isw-inner">
+            </div>
+        </label>
+        <slot name="outer"></slot>
+    </div>
+</template>
+
+<style lang="scss">
+.iswitch-container {
+    display: inline-block;
+
+    .isw-checkbox {
+        display: none;
+    }
+}
+</style>
+
+<script>
+export default {
+    name: 'ISwitch',
+    props: [
+        {
+            name: 'name',
+            type: String
+        },
+        'value',
+        {
+            name: 'size'
+        }
+    ],
+    data() {
+    }
+}
+</script>
